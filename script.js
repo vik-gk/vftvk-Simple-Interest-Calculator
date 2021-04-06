4b61f8c184475f8a1abbd9b41c2fccec2f1986b9
vikranth_gk <gkvikranth@gmail.com>
2021-04-06 10:45:38 +0530

    refined code

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+      
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if(principal <= 0){
+        alert("Please Enter a Positive Number");
+        document.getElementById(principal).focus()
+    }
+    else{
+        var interest = principal * years * rate/100;
+        var updatedYear= end_year = new Date().getFullYear() + years;
+
+        var resultFinal = "If you Deposit <mark>"+principal +"</mark>,"+"<br>At an Interest Rate of <mark>"+rate+
+        "%</mark>,<br>You will Receive an Amount of <mark>"+interest+
+        "</mark>,<br>In the Year <mark>"+updatedYear+"</mark>.";
+
+        document.getElementById("result").innerHTML = resultFinal;
+    }
+
+}
     
+function displayVal(){
+    var rangeVal = document.getElementById("rate").value;
+    document.getElementById("updated_rate").textContent = rangeVal +"%";
 }
         
